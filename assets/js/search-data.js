536722df4a7f805789dffd4ea1c2704f77c1451b
maya-schles <maya.schlesinger@gmail.com>
2025-06-02 14:57:08 +0000

    Deploying to gh-pages from @ maya-schles/maya-schles.github.io@631fbcc61676d1e8abdcbc2b13c2b18ac70c1cb5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-papers",
-          title: "Papers",
+          title: "papers",
           description: "",
           section: "Navigation",
           handler: () => {
